24c532284392da1fa4aa4ba6c74e06bc0a8a8f73
Renata Albuquerque <44981921+renataalbq@users.noreply.github.com>
2021-06-23 17:22:09 -0300

    Aula 3

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,94 @@
+import React, { useState} from 'react';
+import { View, FlatList } from 'react-native';
+
+import { Profile } from '../../components/Profile';
+import { ButtonAdd } from '../../components/ButtonAdd';
+import { CategorySelect } from '../../components/CategorySelect';
+import { ListHeader } from '../../components/ListHeader';
+import { Appointment } from '../../components/Appointment';
+import { ListDivider } from '../../components/ListDivider';
+import { Background } from '../../components/Background';
+
+import { styles } from './styles';
+import { useNavigation } from '@react-navigation/native';
+
+export function Home() {
+  const [category, setCategory] = useState('');
+  const navigation = useNavigation();
+
+  const appointments = [
+    {
+      id: '1',
+      guild: {
+        id: '1',
+        name: 'Lendários',
+        icon: null,
+        owner: true
+      },
+      category: '1',
+      date: '22/06 ás 20:40h',
+      description: 'É hoje que vamos chegar ao challenger sem perder uma partida da md10'
+    },
+    {
+      id: '2',
+      guild: {
+        id: '2',
+        name: 'Quente Novamente',
+        icon: null,
+        owner: true
+      },
+      category: '3',
+      date: '23/06 ás 19:00h',
+      description: 'Platinar o Hero Siege'
+    }
+  ]
+
+  function handleCategorySelect(categoryId: string) {
+    categoryId === category ? setCategory('') : setCategory(categoryId);
+  }
+
+  function handleAppointmentDetails() {
+    navigation.navigate('AppointmentDetails');
+  }
+  
+  function handleAppointmentCreate() {
+    navigation.navigate('AppointmentCreate');
+  }
+
+  return (
+    <Background>
+        <View style={styles.header}>
+          <Profile />
+          <ButtonAdd onPress={handleAppointmentCreate} />
+        </View>
+    
+        <CategorySelect 
+          categorySelected={category}
+          setCategory={handleCategorySelect}
+        />
+
+        <View style={styles.content}>
+          <ListHeader
+            title="Partidas agendadas"
+            subtitle="Total 6"
+          />
+
+          <FlatList
+              data={appointments}
+              keyExtractor={item => item.id}
+              renderItem={({ item }) => (
+                <Appointment 
+                  data={item}
+                  onPress={handleAppointmentDetails}
+                />
+              )}
+              ItemSeparatorComponent={() => <ListDivider />}
+              style={styles.matches}
+              showsVerticalScrollIndicator={false}
+          />
+
+        </View>
+
+    </Background>
+  );
+}
